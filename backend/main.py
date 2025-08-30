31832c61967d3c94a1a7fc197628df9e8981ba0a
Sairamgoud <sairamgoud226@gmail.com>
2025-08-30 10:18:12 +0530

    Adding frontend folder

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from url_checker import check_url   # 👈 imports function from url_checker.py
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -11,3 +12,12 @@ class URLInput(BaseModel):
 def scan_url(data: URLInput):
     result = check_url(data.url)
     return result
+
+# After creating your FastAPI app, add:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
